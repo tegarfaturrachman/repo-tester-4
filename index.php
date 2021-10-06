5147dc29dc3a50508b4e9bfdc4c41309e230a9a6
Tegar Fatur Rachman <tegar6772@gmail.com>
2021-10-06 20:15:13 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, consequuntur voluptatum! Accusantium illum quo vero, unde aliquam consequatur vel? Culpa tenetur officiis labore blanditiis vitae impedit temporibus expedita ducimus rem?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
