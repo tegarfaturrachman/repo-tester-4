4bcfca0b8695c63e44b8ebe3936775bae200efe2
Tegar Fatur Rachman <tegar6772@gmail.com>
2021-10-06 20:09:50 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, consequuntur voluptatum! Accusantium illum quo vero, unde aliquam consequatur vel? Culpa tenetur officiis labore blanditiis vitae impedit temporibus expedita ducimus rem?</p>
 </body>
 </html>
